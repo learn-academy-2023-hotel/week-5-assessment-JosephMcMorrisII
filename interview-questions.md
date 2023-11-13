0bb2c66b2972ba4f6da014f327a8a3fdcaf73b9c
JosephMcMorrisII <josephmcmorris14@gmail.com>
2023-11-13 00:47:41 -0800

    Completed assessment

--- interview-questions.md
+++ interview-questions.md
@@ -8,32 +8,34 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does it mean to implement a TDD workflow?
 
-Your answer:
+Your answer: It means that you should have method that makes your processes run in an optimal way. The TDD helps with this.
 
-Researched answer:
+Researched answer: Creating a full stack application calls for both useful and meaningful data. The TDD workflow ensures useful and meaningful data is stored in the database.
 
 2. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: A relational database let's you pull and push data to other databases that have similar datatype.
 
-Researched answer:
+Researched answer: Relational database is similar to a spreadsheet. The columns and rows describe a single piece of data. So rows and columns are considered relational data which is stored in one location in a relational database which looks like a spreadsheet or a table of data (ie. Query) 
+
+PostgreSQL is another type of database
 
 3. Why is an ORM?
 
-Your answer:
+Your answer: An ORM is a Object range model. It is used to organize and hold data.
 
-Researched answer:
+Researched answer: Stands for Object relational mapping. The function of an ORM is to translate data that are in two different languages. It converts data that is previously not compatible.
 
 4. STRETCH: What is the difference between inheritance in JavaScript and inheritance in Ruby?
 
-Researched answer:
+Researched answer: Ruby uses class baesd model while JavaScript uses prototype-based model with constructor functions. JavaScript supports single inheritance while Ruby supports multiple inheritance.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Model validations:
+1. Model validations: is the process of ensuring the data that is entered is meets certain criteria or constraints.
 
-2. Params:
+2. Params: Params is an input that is passed in a function which changes the output. A strong param provide control over what information is being allowed into a database.
 
-3. API:
+3. API: Stands for Application Programming interface. It allows software applications to communitcate, share data, and utilize each others functionality in a standardized way. The data is stored, retreived, created, updated, or deleted by controll methods.
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -4,22 +4,91 @@
 # -------------------1) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
 
-us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
+# us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
+# def ordered_hash(hash)
+#     flattened_array = hash.values.flatten
+#     sorted_array = flattened_array.sort
+#     return sorted_array
+# end
+
+# puts ordered_hash(us_states)
+
 # Pseudo code:
+# Input: Hash
+# Output: One array with all their values at their own index in alphabethical order.
+# Create a method that takes in a hash
+# Get the values using hash.values and return the new set using flatten
+# Alphabetize the new array with .sort
+
 
 # --------------------2a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.
 
+# class Bike
+#     attr_accessor :model, :wheels, :current_speed
+#     def initialize(model, wheels = 2)
+#       @model = model
+#       @wheels = wheels
+#       @current_speed = 0
+#     end
+#     def bike_info
+#       "The #{model} bike has #{wheels} wheels and is currently going at #{current_speed} mph."
+#     end
+#   end
+
+#   my_bike = Bike.new("Trek bike")
+#   puts my_bike.bike_info
+
 # Pseudo code:
+# Input: Model
+# Output: Model , wheels , MPH
+# Create a class
+# List the attributes you want for the class
+# Initialize, set the wheels = to 2
+# Write the string iterpolation for the return
+
 
 # -------------------2b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
+
+class Bike
+    attr_accessor :model, :wheels, :current_speed
+    def initialize(model, wheels = 2)
+      @model = model
+      @wheels = wheels
+      @current_speed = 0
+    end
+    def bike_info
+      "The #{model} bike has #{wheels} wheels and is currently going at #{current_speed} mph."
+    end
+    def pedal_faster(speed)
+      @current_speed += speed
+    end
+    def brake(speed)
+      @current_speed = [@current_speed - speed, 0].max
+    end
+  end
+
+  my_bike = Bike.new("Trek bike")
+  puts my_bike.bike_info
+  puts my_bike.pedal_faster(10)
+  puts my_bike.pedal_faster(18)
+  puts my_bike.brake(5)
+  puts my_bike.brake(25)
+
+#   The Trek bike bike has 2 wheels and is currently going at 0 mph.
+#   10
+#   28
+#   23
+#   0
 # Expected output example: my_bike.pedal_faster(10) => 10
 # Expected output example: my_bike.pedal_faster(18) => 28
 # Expected output example: my_bike.brake(5) => 23
 # Expected output example: my_bike.brake(25) => 0
 
-# Pseudo code:
+# Pseudo code: 
+#  added another method for pedal faster that increases the 'current speed' by whatever is inputed in the parameter
+#  added another method for brake decreases 'current speed' by whatever speed is inputed in the parameter
